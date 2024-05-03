15ccb2bd7727f709a15901e5c98cebe998919664
austinvirgin <austin.virgin@gmail.com>
2024-05-03 13:37:57 -0600

    done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,27 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        string magicNumberStr;
         string userNumberStr;
         int magicNumber;
         int userNumber;
-        Console.Write("What is the magic number? ");
-        magicNumberStr = Console.ReadLine();
-        magicNumber = int.Parse(magicNumberStr);
-        do
+        string goAgain = "yes";
+        while (goAgain == "yes")
         {
-            Console.Write("What is your guess? ");
-            userNumberStr = Console.ReadLine();
-            userNumber = int.Parse(userNumberStr);
-            if (userNumber > magicNumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            else if (userNumber < magicNumber)
-            {
-                Console.WriteLine("Higher");
-            }
-        } while (userNumber != magicNumber);
-        Console.WriteLine("You guessed it!");
+            Random randomGenerator = new Random();
+            magicNumber = randomGenerator.Next(1, 101);
+            int userGuesses = 0;
+            do {
+                Console.Write("What is your guess? ");
+                userNumberStr = Console.ReadLine();
+                userNumber = int.Parse(userNumberStr);
+                if (userNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                userGuesses++;
+            } while (userNumber != magicNumber);
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {userGuesses} guesses to find the answer.");
+            Console.Write("Would you like to go again? ");
+            goAgain = Console.ReadLine();
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userNumber;
+        int userSum = 0;
+        float userAverage;
+        do {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        } while (userNumber != 0);
+        foreach (int number in numbers)
+        {
+           userSum += number; 
+        }
+        userAverage = ((float)userSum)/numbers.Count;
+        int largest = numbers[0];
+        for (int i = 1; i < numbers.Count; i++)
+        {
+            if (numbers[i] > largest)
+            {
+                largest = numbers[i];
+            }
+        }
+        int smallest = largest;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (0 < numbers[i] && numbers[i] < smallest)
+            {
+                smallest = numbers[i];
+            }
+        }
+        Console.WriteLine($"The sum is: {userSum}");
+        Console.WriteLine($"The average is: {userAverage}");
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallest}");
+        numbers.Sort();
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
