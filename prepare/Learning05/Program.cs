4555dd0be23b50252416ee2b6ade41cd1d15149f
austinvirgin <austin.virgin@gmail.com>
2024-06-19 15:44:20 -0600

    practice

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>(){
+            new Rectangle("red", 2, 4), new Circle("blue", 4), new Square("orange", 5) 
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -1,1 +1,15 @@
-_
+abstract class Shape
+{
+protected Shape(string color){
+    _color = color;
+}
+ protected string _color;
+ public string GetColor(){
+    return _color;
+ }
+ public void SetColor(string color){
+    _color = color;
+ }
+
+ public abstract double GetArea();
+}
