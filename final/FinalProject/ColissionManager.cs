6b4b9579dea0811b87ed4885ab1b8b79cb58b98a
austinvirgin <austin.virgin@gmail.com>
2024-07-11 12:29:19 -0600

    hello world

--- final/FinalProject/ColissionManager.cs
+++ final/FinalProject/ColissionManager.cs
@@ -8,7 +8,9 @@ public class CollisionManager
         {
             if (IsColliding(bird, obstacle))
             {
+                string str = "klsjflkdasjfdskl tilasj";
                 return true;
+                
             }
         }
         return false;
