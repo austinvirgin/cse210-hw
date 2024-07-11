8dc50295f9283ea7c3f763ae46e373d1397e4fb5
austinvirgin <austin.virgin@gmail.com>
2024-07-11 12:23:43 -0600

    done

--- final/FinalProject/Background.cs
+++ final/FinalProject/Background.cs
@@ -1,15 +1,14 @@
 public class Background
 {
-   
+   // public string Texture {get: private set; }
 
     public Background()
     {
-     
 
     }
 
     public void Scroll()
     {
-       
+        //logic to scroll background
     }
 }
