bf36a9a6809f63596d46ce105f39b00ef2c14924
kalebwatts <kalebwatts40@gmail.com>
2024-07-17 10:33:05 -0600

    sound is working!!!

---
+++ final/Foundation1/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstName;
+        string lastName;
+        Console.Write("What is you first name? ");
+        firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");        
+    }
+}
